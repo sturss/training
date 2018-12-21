5aa110dd9b6095827833bc90b4d55bbe82150f25
Sviatoslav Skotar <sturss22@gmail.com>
2018-12-21 11:14:02 +0200

    Minor bug and style fixes

--- producer/api/api_routes/services/kafka.py
+++ producer/api/api_routes/services/kafka.py
@@ -5,12 +5,13 @@ import time
 from kafka import KafkaProducer
 
 from api.logger import logger
+from api.config import Configs
 
 producer = None
 while True:
     try:
-        producer = KafkaProducer(retries=1, bootstrap_servers=['kafka:9092'])
+        producer = KafkaProducer(retries=1, bootstrap_servers=Configs['KAFKA_SERVERS'])
         break
     except Exception as e:
-        logger.error(e)
+        logger.error("Couldn't connect, try in 5 seconds: ", e)
         time.sleep(5)
