5f2723a9644188c5a29d301c9a19bbe271713a9b
Sviatoslav Skotar <sturss22@gmail.com>
2018-12-25 12:09:37 +0200

    Moved engine creation to separate function

--- consumer/api/api_routes/services/postgres_data.py
+++ consumer/api/api_routes/services/postgres_data.py
@@ -7,28 +7,32 @@ from aiopg.sa import create_engine
 from api.config import Configs
 from api.pg_models import Movie
 
+_engine = None
+
+
+async def get_engine():
+    global _engine
+    if not _engine:
+        _engine = await create_engine(
+                user=Configs['POSTGRES_USER'],
+                database=Configs['POSTGRES_DATABASE'],
+                host=Configs['POSTGRES_ADDRESS'],
+                password=Configs['POSTGRES_PASSWORD']
+            )
+    return _engine
+
 
 async def insert_movie(movie):
-    async with create_engine(
-        user=Configs['POSTGRES_USER'],
-        database=Configs['POSTGRES_DATABASE'],
-        host=Configs['POSTGRES_ADDRESS'],
-        password=Configs['POSTGRES_PASSWORD']
-    ) as engine:
-        async with engine.acquire() as conn:
-            async with conn.begin():
-                await conn.execute(Movie.insert().values(movie))
+    engine = await get_engine()
+    async with engine.acquire() as conn:
+        async with conn.begin():
+            await conn.execute(Movie.insert().values(movie))
 
 
 async def get_movies_count():
-    async with create_engine(
-        user=Configs['POSTGRES_USER'],
-        database=Configs['POSTGRES_DATABASE'],
-        host=Configs['POSTGRES_ADDRESS'],
-        password=Configs['POSTGRES_PASSWORD']
-    ) as engine:
-        async with engine.acquire() as conn:
-            async with conn.execute(Movie.select()) as cur:
-                return cur.rowcount
+    engine = await get_engine()
+    async with engine.acquire() as conn:
+        async with conn.execute(Movie.select()) as cur:
+            return cur.rowcount
 
 
