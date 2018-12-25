502fe05ffbeb893fb0b4a4649224c463647fb178
Sviatoslav Skotar <sturss22@gmail.com>
2018-12-25 10:36:34 +0200

    Changed redis for aioredis to make opertaions non-blocking

--- consumer/common/kafka.py
+++ consumer/common/kafka.py
@@ -21,20 +21,20 @@ class Consumer:
     Works with topic movie
     Commits offset to Kafka every 10 seconds or on a new message
     """
-    consumer: AIOKafkaConsumer = None
+    _consumer: AIOKafkaConsumer = None
     _reader_task: asyncio.Task = None
     _commit_task: asyncio.Task = None
     _uncommitted_messages = 0
-    listeners = set()
-    last_commit_time = None
+    _listeners = set()
+    _last_commit_time = None
 
     @classmethod
     def add_listener(cls, listener):
-        cls.listeners.add(listener)
+        cls._listeners.add(listener)
 
     @classmethod
     def remove_listener(cls, listener):
-        cls.listeners.remove(listener)
+        cls._listeners.remove(listener)
 
     @classmethod
     async def connect(cls, on_message=()):
@@ -52,31 +52,28 @@ class Consumer:
         except Exception as e:
             logger.error("Error occurred while trying to close connection with Kafka: %s", e)
         finally:
-            if cls.consumer is not None:
-                await cls.consumer.stop()
-
-    @classmethod
-    async def _get_message_counter(cls):
-        return OffsetStorage.get_value('offset_counter')
+            if cls._consumer is not None:
+                await cls._consumer.stop()
 
     @classmethod
     async def _get_last_offset(cls):
-        return TopicPartition('movie', 0), OffsetStorage.get_value('offset')
+        return TopicPartition('movie', 0), await OffsetStorage.get_value('offset')
 
     @classmethod
     async def _init(cls):
-        OffsetStorage.ensure_record('offset', value=0)
-        OffsetStorage.ensure_record('offset_counter', value=0)
-        cls.consumer = AIOKafkaConsumer('movie',
-                                        group_id="movie_1",
-                                        bootstrap_servers=f"{Configs['KAFKA_ADDRESS']}:{Configs['KAFKA_PORT']}",
-                                        loop=asyncio.get_event_loop(),
-                                        enable_auto_commit=False,
-                                        consumer_timeout_ms=3000
-                                        )
+        await OffsetStorage.ensure_record('offset', value=0)
+        await OffsetStorage.ensure_record('offset_counter', value=0)
+
+        cls._consumer = AIOKafkaConsumer('movie',
+                                         group_id="movie_1",
+                                         bootstrap_servers=f"{Configs['KAFKA_ADDRESS']}:{Configs['KAFKA_PORT']}",
+                                         loop=asyncio.get_event_loop(),
+                                         enable_auto_commit=False,
+                                         consumer_timeout_ms=3000
+                                         )
         while True:
             try:
-                await cls.consumer.start()
+                await cls._consumer.start()
                 logger.info("Connection with Kafka broker successfully established")
                 cls._commit_task = asyncio.ensure_future(cls._interval_commit())
                 break
@@ -84,12 +81,12 @@ class Consumer:
                 logger.error("Couldn't connect to Kafka broker because of %s, try again in 3 seconds", e)
                 await asyncio.sleep(3)
 
-        cls.consumer.seek(*await cls._get_last_offset())
+        cls._consumer.seek(*await cls._get_last_offset())
 
     @classmethod
     async def _interval_commit(cls):
-        if cls.last_commit_time is None:
-            cls.last_commit_time = time.time()
+        if cls._last_commit_time is None:
+            cls._last_commit_time = time.time()
         while True:
             passed_time = time.time() - cls.last_commit_time
             if passed_time < 10:
@@ -97,38 +94,38 @@ class Consumer:
                 if time.time() - cls.last_commit_time < 10:
                     continue
             if cls._uncommitted_messages > 0:
-                await cls.consumer.commit()
+                await cls._consumer.commit()
                 logger.info("Planned interval commit")
                 cls._uncommitted_messages = 0
-            cls.last_commit_time = time.time()
+            cls._last_commit_time = time.time()
 
     @classmethod
     async def _listen(cls):
         await cls._init()
         while True:
             try:
-                message = await cls.consumer.getone()
+                message = await cls._consumer.getone()
                 cls._uncommitted_messages += 1
-                for listener in cls.listeners:
+                for listener in cls._listeners:
                     try:
                         listener(message)
                     except Exception as e:
                         logger.error("Error occurred in Consumer callback function: %s", e)
 
-                OffsetStorage.set_value('offset', OffsetStorage.get_value('offset') + 1)
+                await OffsetStorage.set_value('offset', await OffsetStorage.get_value('offset') + 1)
 
-                counter = (OffsetStorage.get_value('offset_counter') + 1) % 10
-                OffsetStorage.set_value('offset_counter', counter)
+                counter = (await OffsetStorage.get_value('offset_counter') + 1) % 10
+                await OffsetStorage.set_value('offset_counter', counter)
 
                 if counter == 9:
-                    await cls.consumer.commit()
-                    cls.last_commit_time = time.time()
+                    await cls._consumer.commit()
+                    cls._last_commit_time = time.time()
                     cls._uncommitted_messages = 0
                     logger.info("Consumer received 10th message, commit has been performed")
 
             except Exception as e:
                 logger.critical("Unexpected error, wait for 2sec: %s", e)
-                if not cls.consumer:
+                if not cls._consumer:
                     logger.critical("Connection with Kafka lost, try to reconnect")
                     await cls._init()
                 await asyncio.sleep(2)
